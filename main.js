290d6411037def6a3607cd4512f5ad12aa6a814d
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-11-30 16:28:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,26 +15,30 @@ function setup() {
 function modelLoaded() {
     console.log("Model Loaded");
     status = true;
-    objectDetector.detect(img, gotResult);
+    Object_detector.detect(img, gotResult);
 }
 function gotResult(error, results) {
     if(error){
         console.log(error);
     }
     console.log(results);
+
+    objects = results;
 }
 
 function draw() {
     image(img, 0, 0, 650, 450);
-    fill("#51087E");
-    text("Dog", 50, 55);
-    noFill();
-    stroke("#51087E");
-    rect(50, 60, 350, 380 );
-
-    fill("#51087E");
-    text("Cat", 280, 80);
-    noFill();
-    stroke("#51087E");
-    rect(270, 85, 350, 350 );
+    
+    if(status != "")
+    {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object detected";
+             fill("#A020F0");
+             percent = floor(objects[i].confidence * 100);
+             text(objects[i].label + " " + percent + "%", objects[i].x+15, objects[i].y- 5);
+             noFill();
+             stroke("#A020F0");
+             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
